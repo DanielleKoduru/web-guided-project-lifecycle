5e0d1ed692149f39483d06327381871b076df50f
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-03 18:42:22 -0800

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -52,7 +52,8 @@ class App extends React.Component {
       console.log("doggos have changed!");
     }
 
-    // Just for fun, but showing that we can add specific logic in CDU based on state and prop changes
+    // Just for fun, but showing that we can add specific logic in CDU
+    // and trigger additional API requests based on state and prop changes
     if (this.state.dogBreed === "chihuahua") {
       axios.get(`https://dog.ceo/api/breed/mastiff/images`)
       .then(res => {
